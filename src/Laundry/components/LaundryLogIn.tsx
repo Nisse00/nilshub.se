4f69c446235aec95b14ada7fe4b25bbc993ca600
Nils Ekstrand <ekstrand.nils00@gmail.com>
2024-10-24 21:51:06 +0200

    Start of login page

--- src/Laundry/components/LaundryLogIn.tsx
+++ src/Laundry/components/LaundryLogIn.tsx
@@ -1,16 +1,27 @@
+import { useState } from "react";
+
+
 export default function LaundryLogIn() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSubmit(event : React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        console.log(username + " " + password);
+    }
+
     return (
         <div className="d-flex justify-content-center align-items-center vh-100">
             <div className="w-50">
                 <h1 className="text-center">Log in</h1>
-                <form>
+                <form noValidate onSubmit={handleSubmit}>
                     <div className="form-group">
                         <label htmlFor="username">Username</label>
-                        <input type="text" className="form-control" id="username" />
+                        <input type="text" className="form-control" id="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" id="password" />
+                        <input type="password" className="form-control" id="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
                     </div>
                     <button type="submit" className="btn btn-primary w-100">Submit</button>
                 </form>
